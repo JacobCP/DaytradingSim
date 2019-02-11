0d5cafeb7b78f9159f4c59ec8b4c4b526d654ec8
Jacob Ehrlichster <jacobe@retalon.com>
2019-02-11 14:38:09 -0500

    implemented selling, buying and rollover methods
    utilized methods in sim_step

--- sim_functions.py
+++ sim_functions.py
@@ -13,6 +13,7 @@ class Holdings:
         self.num_points = None
         self.shares_to_buy = None
         self.current_index = None
+        self.total_profit = 0
         # historical attributes
         self.hist_capital_available = []
         self.hist_num_positions = []
@@ -42,9 +43,16 @@ class Holdings:
  
     def buy_position(self, price_point_index, current_price):
         shares_to_buy = self.shares_to_buy[price_point_index]
-        self.positions[price_point_index] = [(current_price, shares_to_buy)] # price_point_index: [(price_bought, num_shares_bought)*]
+        self.positions[price_point_index] = (current_price, shares_to_buy) # price_point_index: [(price_bought, num_shares_bought)*]
         self.capital -= current_price * shares_to_buy
 
+    def sell_position(self, price_point_index, current_price):
+        bought_price, shares_bought = self.positions.pop(price_point_index)
+        self.total_profit += (current_price - bought_price) * shares_bought
+
+    def rollover_position(self, from_price_point_index, to_price_point_index):
+        self.positions[to_price_point_index] = self.positions.pop(from_price_point_index)
+    
     def sim_step(self, new_price, new_date_time):
         # some notes
         # for rollover, it needs to reach higher
@@ -52,9 +60,21 @@ class Holdings:
         new_position = self.price_points.searchsorted(new_price)
         if new_position in [self.current_index, self.current_index + 1]: # nothing happened, just move on
             pass
+        
         elif new_position < self.current_index:
-            pass
-            # time to buy a new position
-        else: 
-            pass
-            # time to sell/rollover positions
+            self.buy_position(new_position, new_price)
+            self.current_index = new_position
+        
+        elif new_position > self.current_index + 1: 
+            # get all lower positions
+            lower_positions = [key for key in self.positions if key <= self.current_index]
+
+            # rollover highest one
+            self.rollover_position(lower_positions[-1], new_position)
+
+            # sell all lower ones
+            for position in lower_positions[:-1]:
+                self.sell_position(position, new_price)
+
+            # update current index
+            self.current_index = new_position
