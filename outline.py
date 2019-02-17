2a5bee6c604f476ab1fecfdfb7f399342533acad
Jacob Ehrlichster <jacobe@retalon.com>
2019-02-17 11:47:34 -0500

    changed outline code to looping over growth_step format

--- outline.py
+++ outline.py
@@ -9,18 +9,19 @@ START_DATE_TIME = "2007-10-31 2:58 pm"
 END_DATE_TIME = "2010-12-7 9:30 am"
 CAPITAL_AMOUNT = 1000000
 MAX_EXPECTED_DEPRECIATION_RATE = .60
-GROWTH_STEP_SIZE = .005
+GROWTH_STEP_SIZES = [.005]
 
 # get the high and low prices
 historical_prices = read_hist_data(STOCK_SYMBOL, START_DATE_TIME, END_DATE_TIME) # (ToDo - create read_hist_data functions)
 
-# Create Holdings Instance (automatically initializes first position)
-sim_holdings = Holdings(historical_prices, CAPITAL_AMOUNT, GROWTH_STEP_SIZE, MAX_EXPECTED_DEPRECIATION_RATE)
+for growth_size_step in GROWTH_STEP_SIZES:
+	# Create simulation object
+	sim_holdings = Holdings(historical_prices, CAPITAL_AMOUNT, growth_size_step, MAX_EXPECTED_DEPRECIATION_RATE)
 
-# run the simulation
-sim_holdings.run_sim()
+	# run the simulation
+	sim_holdings.run_sim()
 
-# get sim_info / results
-sim_starting_info = sim_holdings.get_sim_info()
-sim_results_info = sim_holdings.get_results_info()
-full_results = sim_holdings.get_historical()
+	# get sim_info / results
+	sim_starting_info = sim_holdings.get_sim_info()
+	sim_results_info = sim_holdings.get_results_info()
+	full_results = sim_holdings.get_historical()
