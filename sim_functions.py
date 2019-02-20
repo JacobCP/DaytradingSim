47bc36284091d5850e517a1b4ccba0f13e4b336f
Jacob Ehrlichster <jacobe@retalon.com>
2019-02-20 15:53:20 -0500

    refactored __init__ for readability
    factored out create_price_points to be a seperate function
    got rid of unneeded attributes
    moved things around
    renamed attributes

--- sim_functions.py
+++ sim_functions.py
@@ -3,21 +3,25 @@ import pandas as pd
 
 class Holdings:
 
-    def __init__(self, historical_prices, sim_start_date, initial_capital, growth_step_size, max_expected_depreciation_rate, prev_high=None):
-        # get col_names
+    def __init__(self, historical_prices, sim_start_date, initial_capital, growth_step_size, max_expected_depreciation_rate, all_time_high=None):
+        # get col_names to use for readable code
         date_time_col = historical_prices.columns[0]
         price_col = historical_prices.columns[1]
 
-        # before filtering, get the previous high
+        # get the index of the simulation start date - to find the previous high, and afterwards drop previous records
         date_values = historical_prices[date_time_col]
         start_date_index = np.where(date_values == sim_start_date)[0][0]
-        if prev_high is not None:
-            self.high = prev_high
-        else:
-            self.high = np.max(date_values[:start_date_index+1])
-        print("The previous high was {}".format(self.high))
+ 
+        # before filtering, get the previous high
+        def get_previous_high(historical_prices, before_date_index):
+            return np.max(date_values[:before_date_index])
 
-        # drop earlier dates
+        if all_time_high is not None:
+            self.high = all_time_high
+        else:
+            self.high = get_previous_high(historical_prices, start_date_index)
+        print("The previous high was {}".format(self.high))
+        # once we have that, we no longer need the earlier dates
         print("filtering out earlier ones...")
         historical_prices = historical_prices.iloc[start_date_index:,:].copy()
 
@@ -26,43 +30,24 @@ class Holdings:
         self.initial_capital = initial_capital
         self.growth_step_size = growth_step_size		
         self.max_expected_depreciation_rate = max_expected_depreciation_rate
-		# calculate / store additional arguments
-        self.last_historical_index = len(historical_prices) - 1
-        self.start_date_time = historical_prices[date_time_col].iloc[0]
-        self.end_date_time = historical_prices[date_time_col].iloc[-1]		
-        self.capital = initial_capital
-        start_price = historical_prices[price_col].iloc[0]
-        self.lowest_expected_price = start_price * (1 - max_expected_depreciation_rate)
+        
         # other attributes
         self.price_points = None
-        # keep track as simulation moves along
+        # point in time attributes
+        self.capital = initial_capital
         self.positions = {}
-        self.current_index = None
+        self.current_position_index = None
         self.num_positions = 0
-        # historical attributes
+        # historical logging attributes
         self.historical_index = 0
         for column_name in ["capital_available", "num_positions", "step_profit", "step_transactions"]:
             self.historical_data[column_name] = np.nan
 
+        # start the simulation steps
         print("Preparing simulation for step size of {:.1%}...\n".format(growth_step_size))
 
-        # create list of price buy/sell points
-        print("Creating price points, from {} until {}...\n".format(\
-            round(self.lowest_expected_price,2), start_price + .01))
-        price_points = []
-        new_point = round(start_price, 2) + .01 # second to highest price point (current price)
-        growth_step_ratio = 1 + growth_step_size
-        lower_step_ratio = 1 / growth_step_ratio
-        while new_point > self.lowest_expected_price:
-            price_points.insert(0, new_point)
-            new_point = round(new_point * lower_step_ratio, 2)
-        price_points.append(round(price_points[-1] * growth_step_ratio,2)) # highest price point
-        
-        print("Price points created are: \n{}\n".format(price_points))
-        # convert to np.array
-        price_points = np.array(price_points)
-        # store in Class object
-        self.price_points = price_points
+        start_price = historical_prices[price_col].iloc[0]
+        self.price_points = self.create_price_points(start_price, self.max_expected_depreciation_rate, self.growth_step_size)
 
         # initialize first step
         self.first_sim_step()
@@ -94,8 +79,7 @@ class Holdings:
 		# create series of sim information:
         sim_info = pd.Series()
 		# starting parameters
-        sim_info["start"] = self.start_date_time
-        sim_info["end"] = self.end_date_time
+        sim_info["start"] = self.historical_data.iloc[0,0]
         sim_info["capital"] = self.initial_capital
         sim_info["depreciation_accounted_for"] = self.max_expected_depreciation_rate
         sim_info["appreciation_step_size"] = self.growth_step_size
@@ -163,6 +147,34 @@ class Holdings:
         #print("{} (record updated) {}".format(self.historical_index, list(self.historical_data.iloc[self.historical_index])))
 
     ###############################################
+    # price point management
+    ###############################################
+
+    # create list of price buy/sell points
+    def create_price_points(self, start_price, max_expected_depreciation_rate, growth_step_size):
+        lowest_expected_price = start_price * (1 - max_expected_depreciation_rate)
+        print("Creating price points, from {} until {}...\n".format(\
+            round(lowest_expected_price,2), start_price + .01))
+
+        price_points = []
+        
+        new_point = round(start_price, 2) + .01 # second to highest price point (current price)
+        growth_step_ratio = 1 + growth_step_size
+        lower_step_ratio = 1 / growth_step_ratio
+        while new_point > lowest_expected_price:
+            price_points.insert(0, new_point)
+            new_point = round(new_point * lower_step_ratio, 2)
+        price_points.append(round(price_points[-1] * growth_step_ratio,2)) # highest price point
+        
+        # convert to np.array
+        price_points = np.array(price_points)
+
+        print("Price points created are: \n{}\n".format(price_points))
+        
+        return price_points
+
+
+    ###############################################
     # actual simulation steps
     ###############################################
 
@@ -178,7 +190,7 @@ class Holdings:
         initial_index = len(self.price_points) - 2
         initial_price = self.historical_data.iloc[self.historical_index, 1]
         self.buy_position(initial_index, initial_price)
-        self.current_index = initial_index
+        self.current_position_index = initial_index
         # initialize historical information
         self.update_historical(0.0, 1)
         # move historical_index
@@ -206,25 +218,25 @@ class Holdings:
         transactions_made = 0
 
         # do selling / buying / rollover
-        if step_position in [self.current_index, self.current_index + 1]: # nothing happened, just move on
+        if step_position in [self.current_position_index, self.current_position_index + 1]: # nothing happened, just move on
             pass
         
-        elif step_position < self.current_index:
+        elif step_position < self.current_position_index:
             new_position = step_position
 
 			# debugging			
-            #print("{}: {} -> {}".format(debugging_index, self.current_index, new_position))
+            #print("{}: {} -> {}".format(debugging_index, self.current_position_index, new_position))
             #print("positions: {}".format(sorted(self.positions)))
 
             self.buy_position(new_position, new_price)
-            self.current_index = new_position
+            self.current_position_index = new_position
             transactions_made += 1
 
-        elif step_position > self.current_index + 1: 
+        elif step_position > self.current_position_index + 1: 
             new_position = step_position - 1
 
 			# debugging			
-            #print("{}: {} -> {}".format(debugging_index, self.current_index, new_position))
+            #print("{}: {} -> {}".format(debugging_index, self.current_position_index, new_position))
             #print("positions: {}".format(sorted(self.positions)))
 
             # get all lower positions
@@ -244,7 +256,7 @@ class Holdings:
                 self.rollover_position(lower_positions[-1], new_position)
 
             # update current index
-            self.current_index = new_position
+            self.current_position_index = new_position
             #print("{} (end of sales): profit made was: {:.2f}".format(debugging_index, profit_made))
 
 
