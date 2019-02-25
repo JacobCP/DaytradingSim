273df2e8066e1b353601b40f0ae39ee8fff53c66
Jacob Ehrlichster <jacobe@retalon.com>
2019-02-25 12:58:32 -0500

    changed parameters for sample simulation

--- outline.py
+++ outline.py
@@ -4,11 +4,12 @@ from data_in_out import read_hist_data, save_results
 import pandas as pd
 
 # choose parameters
-STOCK_SYMBOL = "QQQ" 
-START_DATE_TIME = "2007-10-31 2:58 pm"
+STOCK_SYMBOL = "IVV" 
+START_DATE_TIME = "2003-01-02 09:32:00" # beginning of 2002
+MIN_MONTH_DURATION = 13 * 12 # till end of 2015
 CAPITAL_AMOUNT = 1000000
 MAX_EXPECTED_DEPRECIATION_RATE = .60
-GROWTH_STEP_SIZES = [.1]
+GROWTH_STEP_SIZES = np.linspace(.002,.1,50)
 
 # get the high and low prices
 historical_prices = read_hist_data(STOCK_SYMBOL)
@@ -19,7 +20,7 @@ compare_results = pd.DataFrame(columns = compare_results_columns)
 
 for growth_size_step in GROWTH_STEP_SIZES:
 	# Create simulation object
-	sim_holdings = Holdings(historical_prices, START_DATE_TIME, CAPITAL_AMOUNT, growth_size_step, MAX_EXPECTED_DEPRECIATION_RATE, min_months_duration=10)
+	sim_holdings = Holdings(historical_prices, START_DATE_TIME, CAPITAL_AMOUNT, growth_size_step, MAX_EXPECTED_DEPRECIATION_RATE, min_months_duration=MIN_MONTH_DURATION)
 
 	# run the simulation
 	sim_holdings.run_sim()
